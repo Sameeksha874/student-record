ac3e0f5c1d3e0c897a7345fa4c334d365e966a9b
Sameeksha874 <sameekshashetty2006@gmail.com>
2025-09-25 11:02:47 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,45 +1,48 @@
+const studentform=document.getElementById('studentform');
+const studentlist=document.getElementById('studentlist');
+const errorDiv=document.getElementById('error');
+const students=[];
+studentform.addEventListener('submit',function(event)
+{
+    event.preventDefault();
+const studentId=document.getElementById('studentId').value;
+const studentName=document.getElementById('studentName').value;
+const studentAge=document.getElementById('studentAge').value;
 
-    const studentForm = document.getElementById('student form');
-    const studentList = document.getElementById('studentList');
-    const errorDiv = document.getElementById('error');
-    const students = [];
+try{
+    addstudent(studentId,studentName,studentAge);
+    displaystudents();
+    errorDiv.textContent='';
+}
+catch(error){
+ errorDiv.textContent=error.message;
+}
+studentform.reset();
+});
+function addstudent(id,name,age){
+    if(!id|| !name || !age){
+        throw new Error("All fields are required");
+    }
+  if(isNaN(age) || age<=0){
+    throw new Error("Age must be a positive number");
+  }  
+  const studentExists = students.some(student=>student.id===id);
+  if(studentExists){
+    throw new Error("Student Id Already exists");
+  }
+  const student={
+    id,
+    name,
+    age:parseInt(age,10)
+  };
+  students.push(student);
+  }
+  function displaystudents(){
+    studentlist.innerHTML='';
+    students.forEach(student=>{
+        const li=document.createElement('li');
+        li.textContent= `ID: ${student.id}, Name:${student.name},Age:${student.age}`;
+        studentlist.appendChild(li);
 
-    studentForm.addEventListener('submit', function(event) {
-        event.preventDefault();
-        const studentID = document.getElementById('studentID').value;
-        const studentName = document.getElementById('name').value;
-        const studentAge = document.getElementById('age').value;
-        try{
-            addstudent(studentID, studentName, studentAge);
-            displayStudents();
-            errorDiv.textContent = '';
-        }catch(error){
-            errorDiv.textContent = error.message;
-        }
-        studentForm.reset();
     });
-    function addstudent(id, name, age) {
-        if (!id || !name || !age) {
-            throw new Error('All fields are required');
-        }
-        if (isNaN(age) || age <= 0) {
-            throw new Error('Age must be a positive number');
-        }
-        const studentexists = students.some(student => student.id === id);
-
-        if (studentexists) {
-            throw new Error('Student ID must be unique');
-        }
-        const student = { id, name, age:parseInt(age,10) 
-        };
-        students.push(student);
-    }
-    function displayStudents() {
-        studentList.innerHTML = '';
-        student.forEach(student => {
-            const li = document.createElement('li');
-            li.textContent = `ID: ${student.id}, Name: ${student.name}, Age: ${student.age}`;
-            studentList.appendChild(li);
-        });
-    }
-        
+  }
